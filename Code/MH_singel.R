0835d5e7f284c02e82e50030514f0443c8e67751
Aurora <aurora-1@live.com>
2021-03-12 14:52:40 +0100

    singel cite working

--- Code/MH_singel.R
+++ Code/MH_singel.R
@@ -8,32 +8,41 @@
 #   return(exp(-1/beta * (lambda_0 + lambda_1 + 1))*(1/beta)^5)
 # }
 
-f_cond_t <- function(beta, lambda_0, lambda_1, y_0, y_1){
-  return(lambda_0^{y_0+1} *lambda_1^{y_1+1}*exp(-t* (lambda_0 - lambda_1 )))
-}
-
 MH_alg <- function(n,data, t_0,t_2, t, lambda_0,lambda_1, beta, sigma){
 
   y_0 = sum(data[date <t]$event)
   y_1 = sum(data[date>= t]$event)
   y_0
   y_1
-  results <- matrix(nrow = n, ncol = 4)
-  results[1, 1:4] <- c(t, lambda_0, lambda_1, beta)
+  
+  t = t
+  t
+  beta = beta
+  results <- matrix(nrow = n, ncol = 5)
+  results[1, 1:5] <- c(t, lambda_0, lambda_1, beta, 1)
   for(i in 2:n){
+    print(i)
     results[2:10, 1:4]
     #print("t")
     #print(results[i-1,1])
-    y_0 = sum(data[date <results[i-1,1]]$event)
-    y_1 = sum(data[date>= results[i-1,1]]$event)
+    t_old <- results[i-1, 1]
+    
+    y_0_o = sum(data[date <t_old]$event)
+    y_1_o = sum(data[date>= t_old]$event)
     #print(i)
     #gibs step
     
-    lambda_0 <- rgamma(1, (y_0 + 2 ), scale= (1/(t - t_0 + 1/beta))) #stemmer shape param?
-    lambda_1 <- rgamma(1, (y_1 + 2 ), scale=(1/(t_2 - t + 1/beta))) #stemmer shape param?
+    beta <- results[i-1, 4]
+    beta
+    
+    lambda_0 <- rgamma(1, y_0 + 2, scale = 1/(t_old - t_0 + 1/beta))
+    lambda_1 <- rgamma(1, y_1 + 2, scale = 1/(t_2 - t_old + 1/beta))
+    
+    
     lambda_0
     lambda_1
-    beta_tilde <- rgamma(1, 6, scale = 1/(lambda_0 + lambda_1 + 1))
+    beta_tilde <- rgamma(1, 6, scale = 1/(lambda_0 + lambda_1 + 1)) #is this okay to d0?
+    beta_tilde
     beta <- 1/beta_tilde
     beta
     results[i, 2:4] <- c( lambda_0, lambda_1, beta)
@@ -43,13 +52,28 @@ MH_alg <- function(n,data, t_0,t_2, t, lambda_0,lambda_1, beta, sigma){
     t_old <- results[i-1, 1]
     t_new <- rnorm(1, t_old, sigma)
     t_new
+    print(t_new)
+    
+    y_0_n = sum(data[date <t_new]$event)
+    y_1_n = sum(data[date>= t_new]$event)
     #print(c("T_old", t_old, "T_new", t_new))
     ## acceptannce prob
-    a <- min(1, (lambda_0^{y_0+1} *lambda_1^{y_1+1}*exp(-t_new *(lambda_0 - lambda_1 )))/(lambda_0^{y_0+1} *lambda_1^{y_1+1}*exp(-t_old *(lambda_0 - lambda_1 ))))
-    a
+    
+    a_log <- (y_0_n +1)*log(lambda_0) + (y_1_n +1)*log(lambda_1) - t_new *(lambda_0 - lambda_1 ) - ((y_0_o +1)*log(lambda_0) + (y_1_o +1)*log(lambda_1) - t_old *(lambda_0 - lambda_1 ))
+    
+    a_log
+    
+#    a <-  (lambda_0^{y_0_n+1} *lambda_1^{y_1_n+1}*exp(-t_new *(lambda_0 - lambda_1 )))/(lambda_0^{y_0_o+1} *lambda_1^{y_1_o+1}*exp(-t_old *(lambda_0 - lambda_1 )))
+
+ #   print(a)
+    
+    exp(a_log)
+    
+    
     u <- runif(1)
     u
-    if (u< a){
+    results[i, 5] <-  a
+    if (u< exp(a_log)){
       results[i, 1] <-  t_new
     } else{
       results[i,1] <- t_old
@@ -76,7 +100,7 @@ t = 45
 
 lambda_0 <- 10 #135/50
 lambda_1 <- 5 #56/50
-beta = 10
+beta = 3
 
 n = 10000
 t_0 = 0
@@ -88,7 +112,7 @@ sim_MH <- MH_alg(n,data, t_0, t_2, t, lambda_0,lambda_1, beta, sigma)
 
 sim_MH <- as.data.table(sim_MH)
 
-setnames(sim_MH, c("t", "lambda_0", "lambda_1", "beta"))
+setnames(sim_MH, c("t", "lambda_0", "lambda_1", "beta", "a"))
 
 sim_MH[, itteration := seq(1:n)]
 sim_MH
